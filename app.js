a83e69df29070b8f5cb36597c341fb5acdced41d
John Franzese <johnfranzese21@gmail.com>
2024-02-26 15:08:38 -0600

    JS Functions

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    if (count < 0) {
+        console.log("a non-negative count.");
+        return;
+    }
+
+    for (let i = 1; i <= count; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
+    }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
